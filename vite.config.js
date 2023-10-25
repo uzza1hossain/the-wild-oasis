156d5bfe194e1b7c728114a60a284a2e2c12d0ea
Uzzal Hossain <de3dm3nu11@gmail.com>
2023-10-25 22:52:16 +0600

    add booking details page

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import fs from 'fs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    https: {
+      key: fs.readFileSync('/Users/uzzal/localhost+2-key.pem'),
+      cert: fs.readFileSync('/Users/uzzal/localhost+2.pem'),
+    },
+  },
   plugins: [react()],
-})
+});
