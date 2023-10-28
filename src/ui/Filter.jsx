3c8813b0c839c6eb44c8fa16de75fb90e0ceae59
Uzzal Hossain <de3dm3nu11@gmail.com>
2023-10-28 20:31:36 +0600

    update filter page number to solve range not satisfied error

--- src/ui/Filter.jsx
+++ src/ui/Filter.jsx
@@ -40,6 +40,7 @@ function Filter({ filterField, options }) {
   const currentFilter = searchParams.get(filterField) || options.at(0).value;
   function handleClick(value) {
     searchParams.set(filterField, value);
+    if (searchParams.get('page')) searchParams.set('page', 1);
     setSearchParams(searchParams);
   }
   return (
@@ -48,7 +49,7 @@ function Filter({ filterField, options }) {
         <FilterButton
           onClick={() => handleClick(option.value)}
           key={option.value}
-          active={option.value === currentFilter? "true": ""}
+          active={option.value === currentFilter ? 'true' : ''}
           disabled={option.value === currentFilter}>
           {option.label}
         </FilterButton>
