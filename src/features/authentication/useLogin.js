5b1f8c4a80337ca97f8334178ccf123837852e3f
Uzzal Hossain <de3dm3nu11@gmail.com>
2023-10-29 11:43:22 +0600

    add login functionallity

---
+++ src/features/authentication/useLogin.js
@@ -0,0 +1,19 @@
+import { useMutation } from '@tanstack/react-query';
+import { login as loginApi } from '../../services/apiAuth';
+import { useNavigate } from 'react-router-dom';
+import toast from 'react-hot-toast';
+
+export function useLogin() {
+  const navigate = useNavigate();
+  const { mutate: login, isLoading } = useMutation({
+    mutationFn: ({ email, password }) => loginApi({email, password}),
+    onSuccess: (user) => {
+      navigate('/dashboard');
+    },
+    onError: (err) => {
+      toast.error('Invalid email or password.');
+    },
+  });
+
+  return { login, isLoading };
+}
