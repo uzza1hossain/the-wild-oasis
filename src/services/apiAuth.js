123dfe7625f21c77fc3d18afa05f01a018a3f359
Uzzal Hossain <de3dm3nu11@gmail.com>
2023-11-04 23:28:21 +0600

    fix profile photo update delay

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -56,7 +56,7 @@ export async function updateCurrentUser({ password, fullName, avatar }) {
     .upload(fileName, avatar);
   if (storageError) throw new Error(storageError.message);
 
-  const { data: updatedUser, error: error2 } = supabase.auth.updateUser({
+  const { data: updatedUser, error: error2 } = await supabase.auth.updateUser({
     data: {
       avatar: `${supabaseUrl}/storage/v1/object/public/avatars/${fileName}`,
     },
