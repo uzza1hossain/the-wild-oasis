997fd678bf17131f2939f567c8201d21e89efbe7
Uzzal Hossain <de3dm3nu11@gmail.com>
2023-11-05 11:39:32 +0600

    calculate statistics

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,4 +1,7 @@
-import styled from "styled-components";
+import styled from 'styled-components';
+import Spinner from '../../ui/Spinner';
+import { useRecentBookings } from './useRecentBookings';
+import { useRecentStays } from './useRecentStays';
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -6,3 +9,25 @@ const StyledDashboardLayout = styled.div`
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+function DashboardLayout() {
+  const { bookings, isLoading1 } = useRecentBookings();
+  const { isLoading2, stays, confirmedStays } = useRecentStays();
+  if (isLoading1 || isLoading2) return <Spinner />;
+  console.log(bookings);
+  console.log(stays);
+  console.log(confirmedStays);
+  // console.log(`Bookings within days: ${bookings}`);
+  // console.log(`Stays: ${stays}`);
+  // console.log(`Confirm stays: ${confirmedStays}`);
+  return (
+    <StyledDashboardLayout>
+      <div>Statistics</div>
+      <div>Today's activity</div>
+      <div>Chart stay duration</div>
+      <div>Chart sales</div>
+    </StyledDashboardLayout>
+  );
+}
+
+export default DashboardLayout;
