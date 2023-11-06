48845cd64e7575626002aa5f009ade254128d80e
Uzzal Hossain <de3dm3nu11@gmail.com>
2023-11-06 20:01:32 +0600

    remove dev user pass

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -6,10 +6,9 @@ import FormRowVertical from '../../ui/FormRowVertical';
 import SpinnerMini from '../../ui/SpinnerMini';
 import { useLogin } from './useLogin';
 
-
 function LoginForm() {
-  const [email, setEmail] = useState('uzzal@example.com');
-  const [password, setPassword] = useState('testpass123');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const { login, isLoading } = useLogin();
 
   function handleSubmit(e) {
