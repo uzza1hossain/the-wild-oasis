d12e52fd6811451a083dc48446c027a420a82395
Uzzal Hossain <de3dm3nu11@gmail.com>
2023-11-06 20:53:01 +0600

    remove local ssl

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,5 @@ import fs from 'fs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    https: {
-      key: fs.readFileSync('/Users/uzzal/localhost+2-key.pem'),
-      cert: fs.readFileSync('/Users/uzzal/localhost+2.pem'),
-    },
-  },
   plugins: [react()],
 });
